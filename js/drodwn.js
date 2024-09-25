67a959bf71b4b126cdab603abea4b6411b891ad4
Astrum <astrum@iMac-Astrum-214.local>
2024-09-25 19:58:38 +0500

    Added something

--- js/drodwn.js
+++ js/drodwn.js
@@ -1,19 +1,19 @@
-document.querySelectorAll('.dropbtn').forEach(button => {
-    button.onclick = function(event) {
-        const dropdownMenu = this.nextElementSibling;
-        const isOpen = dropdownMenu.style.display === 'block';
-        dropdownMenu.style.display = isOpen ? 'none' : 'block';
-        this.textContent = isOpen ? '+' : '-';
-    };
-});
+// document.querySelectorAll('.dropbtn').forEach(button => {
+//     button.onclick = function(event) {
+//         const dropdownMenu = this.nextElementSibling;
+//         const isOpen = dropdownMenu.style.display === 'block';
+//         dropdownMenu.style.display = isOpen ? 'none' : 'block';
+//         this.textContent = isOpen ? '+' : '-';
+//     };
+// });
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        document.querySelectorAll('.dropdown-content').forEach(menu => {
-            menu.style.display = 'none';
-        });
-        document.querySelectorAll('.dropbtn').forEach(button => {
-            button.textContent = '+';
-        });
-    }
-};
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         document.querySelectorAll('.dropdown-content').forEach(menu => {
+//             menu.style.display = 'none';
+//         });
+//         document.querySelectorAll('.dropbtn').forEach(button => {
+//             button.textContent = '+';
+//         });
+//     }
+// };
